81fd8a214eeeb12a548329291390be3f975b9ba3
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2015-03-20 21:37:44 +0100

    Added new test for NewsLoader. Check if news factory reader is invoked with proper parameter.

--- src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
+++ src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
@@ -10,6 +10,8 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.mockito.internal.verification.VerificationModeFactory;
+import org.mockito.verification.VerificationMode;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -56,6 +58,33 @@ public class NewsLoaderTest {
 		PublishableNews publishableNews = newsLoader.loadNews();
 		assertThat(publishableNews.getSubscribentContent().size(), is(4));
 		assertThat(publishableNews.getPublicContent().size(), is(2));
+	}
+	
+	@Test
+	public final void testLoadNews_newsFaktoryReaderIsInvokedWithProperParameter() throws Exception {
+		// given
+		PowerMockito.mockStatic(ConfigurationLoader.class);
+		PowerMockito.mockStatic(NewsReaderFactory.class);
+		NewsReaderFactory newsReaderFactory = Mockito.mock(NewsReaderFactory.class);
+		ConfigurationLoader configurationLoader = Mockito.mock(ConfigurationLoader.class);
+		Configuration configuration = Mockito.mock(Configuration.class);
+		NewsReader newsReader = new FakeNewsReader();
+
+		// when
+		Mockito.when(ConfigurationLoader.getInstance()).thenReturn(configurationLoader);
+		Mockito.when(configurationLoader.loadConfiguration()).thenReturn(configuration);
+		Mockito.when(NewsReaderFactory.getReader(Mockito.anyString())).thenReturn(newsReader);
+
+		
+		Mockito.when(configuration.getReaderType()).thenReturn("OWN_TYPE");
+		PowerMockito.whenNew(NewsReaderFactory.class).withNoArguments().thenReturn(newsReaderFactory);
+		
+		// then
+		NewsLoader newsLoader = new NewsLoader();
+		PublishableNews publishableNews = newsLoader.loadNews();
+		PowerMockito.verifyStatic();
+		NewsReaderFactory.getReader("OWN_TYPE");
+
 	}
 
 }
