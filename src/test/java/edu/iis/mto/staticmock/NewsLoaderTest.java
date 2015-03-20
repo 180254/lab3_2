aada65c7add6f41554d8da90208129105871d027
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2015-03-20 21:06:17 +0100

    Bug fix for Publishable News. addForSubscription was not implemented. Bug fouund while writing NewsLoader test. / Also: Organized NewsLoaderTest imports.

--- src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
+++ src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
@@ -1,77 +1,23 @@
 package edu.iis.mto.staticmock;
 
-import static org.junit.Assert.*;
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-
-
-
-import static org.hamcrest.core.Is.*;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
-
-import java.util.Date;
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.mockito.Mockito;
-
 import static org.hamcrest.core.Is.is;
-
-import  org.hamcrest.core.*;
-
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
-
-import java.util.Date;
-
-import junit.framework.Assert;
 
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Test;
-import org.mockito.Mockito;
-
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import static org.powermock.api.mockito.PowerMockito.*;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.verify;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mockito;
-import org.powermock.api.mockito.*;
-
 import edu.iis.mto.staticmock.reader.NewsReader;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ConfigurationLoader.class, NewsReaderFactory.class} )
+@PrepareForTest({ ConfigurationLoader.class, NewsReaderFactory.class })
 public class NewsLoaderTest {
 
 	@BeforeClass
@@ -92,19 +38,19 @@ public class NewsLoaderTest {
 
 	@Test
 	public final void testLoadNews_areIncomingNewsProperlySegragated() {
-		//given
+		// given
 		PowerMockito.mockStatic(ConfigurationLoader.class);
 		PowerMockito.mockStatic(NewsReaderFactory.class);
 		ConfigurationLoader configurationLoader = Mockito.mock(ConfigurationLoader.class);
 		Configuration configuration = Mockito.mock(Configuration.class);
 
 		NewsReader newsReader = new FakeNewsReader();
-		
+
 		// when
 		Mockito.when(ConfigurationLoader.getInstance()).thenReturn(configurationLoader);
 		Mockito.when(configurationLoader.loadConfiguration()).thenReturn(configuration);
 		Mockito.when(NewsReaderFactory.getReader(Mockito.anyString())).thenReturn(newsReader);
-		
+
 		// then
 		NewsLoader newsLoader = new NewsLoader();
 		PublishableNews publishableNews = newsLoader.loadNews();
